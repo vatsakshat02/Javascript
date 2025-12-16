f22d77a88da1e67bbe296da99518e202026fea34
vatsakshat02 <vatsakshat02@gmail.com>
2025-12-16 17:07:00 +0530

    other: string manipulation

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 // var let const
 //var is globally constant so we don't use it so basically let is the variable 
-let age = 30;
- console.log(age)
+
+
+ //data types in javascript are String, Numbers, Boolean, null, undefined
+ const name = 'akshat';
+ const age = 21;
+ console.log(typeof name);
+
+ //concatenation
+ console.log('My name is  ' + name + ' and my age is ' + age)
+ //template String
+ console.log(`my name is 4 ${ name } and my age is ${age}`)
+
+ //String manipulation
+ const s ="akshat vats"
+ console.log(s.length);
